59236a8d3937822bd70b1c37d95ffcc26f19dbc9
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 <vgobin@outlook.com>
2022-09-15 10:00:12 -0700

    Added file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * main - print line "Programming is like building a multilingual puzzle.
+ * Return: 0 if correctly exited.
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle);
+	return (0);
+}
