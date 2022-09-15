5b1a0842197c948368f9eac3e606fb7b218c33fb
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 <vgobin@outlook.com>
2022-09-15 10:30:19 -0700

    Added file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ * main -  prints the size of various types on the computer
+ * it is compiled and run on 
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Size of a char: %d\n" ,sizeof(char*));
+	printf("Size of an int: %d\n" ,sizeof(int*));
+       	printf("Size of a long int: %d\n" ,sizeof(long int*));
+	printf("Size of a long long int: %d\n" ,sizeof(long long int*));
+	printf("Size of a float: %d\n" ,sizeof(float*));
+	return (0);	
+}
