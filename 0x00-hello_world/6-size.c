f11aa4364d8f6d13469e077fc872076b78b465b4
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 <vgobin@outlook.com>
2022-09-15 11:21:44 -0700

    Updated file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-/*
- * main -  prints the size of various types on the computer
- * it is compiled and run on
- * Return: 0
+/**
+ * main - prints the size of various types on the computer,
+ * it is compiled and run on.
+ * Return: 0.
  */
 int main(void)
 {
