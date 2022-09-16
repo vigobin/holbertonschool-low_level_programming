8f22f5e70e2264e8e7b25591eef2c1e191596838
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 <vgobin@outlook.com>
2022-09-16 07:50:47 -0700

    Added file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+ * main - program that prints the alphabet in lowercase.
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'A' ; ch, = 'Z' ; ch++)
+		putchar(ch)
+	return (0);
+
+}
