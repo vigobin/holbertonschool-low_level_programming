0f5d49cc6e0cac47a96e1e25d6ec5a76370f3738
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 <vgobin@outlook.com>
2022-09-19 07:18:25 -0700

    Added file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints _putchar, followed by a new line.
+ * Return: Program should return 0.
+ */
+
+int main(void)
+{
+	printf ("_putchar");
+	return (0);
+}
