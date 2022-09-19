913f159f0b8f8d302aa65674f2c0fe1763941c29
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 <vgobin@outlook.com>
2022-09-19 09:36:06 -0700

    Updated file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,13 +8,19 @@
 int print_sign(int n)
 {
 	if ( n > 0 )
+	{
 		return (1);
 		_putchar('+');
+	}
 	else if ( n == 0 )
+	{
 		return (0);
 		_putchar('0');
+	}
 	else if ( n < 0 )
+	{
 		return (-1);
 		_putchar('-');
+	}
 
 }
