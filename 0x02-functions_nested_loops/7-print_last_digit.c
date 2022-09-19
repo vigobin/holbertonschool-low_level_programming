0902330951ac1705bc155a977dd7f23f76f7e344
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 <vgobin@outlook.com>
2022-09-19 12:11:08 -0700

    Added file

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Function that prints the last digit of a number.
+ * Return: 0
+ */
+
+int print_last_digit(int n)
+{
+	int remainder;
+	remainder = n % 10;
+	return (remainder);
+}
