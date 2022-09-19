ecbb815996720b5748d690a38e64b3684b000544
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 <vgobin@outlook.com>
2022-09-19 08:46:57 -0700

    Added file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#incude "main.h"
+
+/**
+ * main - Function that prints 10 times the alphabet.
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	for (count = 0 ; count < 10 ; count++)
+	{
+		char ch = 'a';
+
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+	}
+	_putchar('\n');
+}
