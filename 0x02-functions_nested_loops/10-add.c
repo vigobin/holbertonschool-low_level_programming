a92b9e9ccd34b40f1459b898c27087b505e0db6f
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 <vgobin@outlook.com>
2022-09-20 11:27:35 -0700

    Added file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Function that adds two integers and returns the result.
+ * @a : first number.
+ * @b : second numer.
+ * Return: sum of first and second numbers.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
