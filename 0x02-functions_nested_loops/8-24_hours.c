9d2626475330c63eb06667cd12aaf81ee647676c
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 <vgobin@outlook.com>
2022-09-20 09:27:11 -0700

    Updated file for Betty style

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - function that prints every minute of the day of Jack Bauer
+ * jack_bauer - function that prints every minute of the day of Jack Bauer
  * starting from 00:00 to 23:59.
  * Return: 0
  */
