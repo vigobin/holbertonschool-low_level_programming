c4e7de76034d386b14c18eab3fdcd9fc86c9009a
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 <vgobin@outlook.com>
2022-09-20 12:19:11 -0700

    Updated file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,21 +11,25 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		while (n <= 98)
+		while (n < 98)
 		{
 			printf("%d", n);
 			printf(", ");
+			n++;
 		}
-		n++;
 	}
 	else if (n > 98)
 	{
-		while (n >= 98)
+		while (n > 98)
 		{
 			printf("%d", n);
 			printf(", ");
+			n--;
 		}
 	}
 	else
+	{
 		printf("98");
+	}
+	printf("\n");
 }
