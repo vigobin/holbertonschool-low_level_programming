92574c9b24e2d8c8f194103c40cd62231a12b88f
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 <vgobin@outlook.com>
2022-09-20 07:29:43 -0700

    Updated file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,6 +9,10 @@ int print_last_digit(int n)
 {
 	int remainder;
 	remainder = (n % 10);
+	if (remainder < 0)
+	{
+		remainder = (-1 * remainder);
+	}
 	_putchar(remainder + '0');
 
 	return (remainder);
