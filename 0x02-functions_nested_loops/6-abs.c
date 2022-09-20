b82fb33e34b41850e15eec2ad8280ab38f0277a2
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 <vgobin@outlook.com>
2022-09-20 09:14:43 -0700

    Updated file for Betty style

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * main - Function that computes the absolute value of an integer.
+ * _abs - Function that computes the absolute value of an integer.
+ * @n : integer input.
  * Return: 0
  */
 
 int _abs(int n)
 {
 
-	if ( n < 0 )
+	if (n < 0)
 	{
 		return (-n);
 	}
