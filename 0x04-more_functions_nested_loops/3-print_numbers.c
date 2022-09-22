e0d2194d34c8b2081c882f89057ff04b5876667f
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 <vgobin@outlook.com>
2022-09-22 08:07:08 -0700

    Added file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - Function that prints the numbers, from 0 to 9.
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	for (n = '0'; n <= '9'; n++)
+		_putchar(n);
+	_putchar(\n);
+	return (0);
+}
