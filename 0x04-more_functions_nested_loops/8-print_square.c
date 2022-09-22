bd92b5ae27832662cb2c56c3f577b2745de0a1da
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 <vgobin@outlook.com>
2022-09-22 10:05:59 -0700

    Updated file

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_square - Function that prints a square.
+ * @size : size input. 
+ * Return: display square.
+ */
+
+void print_square(int size)
+{
+	int i;
+
+	if (size > 0)
+	{
+		for(i = 0; i < size; i++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
