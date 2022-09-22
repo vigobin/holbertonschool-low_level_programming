98508bd13184b80a0d7be3b2e03e7b4646aff15d
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 <vgobin@outlook.com>
2022-09-22 08:18:51 -0700

    Updated variables

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,14 @@
 
 /**
  * print_numbers - Function that prints the numbers, from 0 to 9.
- * Return: 0
+ * Return: numbers.
  */
 
 void print_numbers(void)
 {
 	int n;
 
-	for (n = '0'; n <= '9'; n++)
-		_putchar(n);
-	_putchar(\n);
-	return (0);
+	for (n = 0; n <= 9; n++)
+		_putchar(n + '0');
+	_putchar('\n');
 }
