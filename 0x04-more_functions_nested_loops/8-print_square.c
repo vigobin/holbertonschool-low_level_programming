45704a1bed88682e31fa132d4ca93d8dac6a36bf
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 <vgobin@outlook.com>
2022-09-23 08:28:38 -0700

    Updated counter

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,15 +8,16 @@
 
 void print_square(int size)
 {
-	int i,j;
+	int i, j;
 
 	if (size > 0)
 	{
 		for (i = 0; i < size; i++)
 		{
-			_putchar('#');
-			for (j = 1; j < size; i++)
+			for (j = 0; j < size; i++)
+			{
 				_putchar('#');
+			}
 			_putchar('\n');
 		}
 	}
