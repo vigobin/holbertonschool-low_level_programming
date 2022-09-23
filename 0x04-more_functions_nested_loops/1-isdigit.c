4b9b4f6e81c4766d081c18580f4b5911024f1bcc
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 <vgobin@outlook.com>
2022-09-23 07:07:34 -0700

    Betty style format

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,13 +2,13 @@
 
 /**
  * _isdigit - Function that checks for a digit (0 through 9).
- * @c : number input
+ * @c : number input.
  * Return: 1 is c is a digit, 0 otherwise.
  */
 
 int _isdigit(int c)
 {
-	if (c >= '1' && c <= '9')
+	if ((c >= '1') && (c <= '9'))
 		return (1);
 	else
 		return (0);
