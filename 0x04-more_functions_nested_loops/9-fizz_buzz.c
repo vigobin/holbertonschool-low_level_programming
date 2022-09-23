2470920be9100eff3453fe4308167f02acec56d5
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 <vgobin@outlook.com>
2022-09-23 08:46:36 -0700

    Added space

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,7 +14,7 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 != 0)
 		{
 			printf(" Fizz");
 		}
@@ -22,7 +22,7 @@ int main(void)
 		{
 			printf(" Buzz");
 		}
-		else if ((i % 3 == 0) && (i % 5 == 0))
+		else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf(" FizzBuzz");
 		}
