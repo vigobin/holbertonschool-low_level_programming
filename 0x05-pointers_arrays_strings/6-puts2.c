271d3002c83d61450eb01409f9fa450d9451af1d
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 <vgobin@outlook.com>
2022-09-26 13:20:08 -0700

    Added file

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints every other character of a string,
+ * starting with the first character.
+ * @str : input string.
+ * Return: print characters.
+ */
+
+void puts2(char *str)
+{
+
+	for (int length = 0; str[length] <= '\0'; length++)
+	{
+		if (length % 2 == 0)
+			_putchar(str[length]);
+	}
+	_putchar('\n');
+}
