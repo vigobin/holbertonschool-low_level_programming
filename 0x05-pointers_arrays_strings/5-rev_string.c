af59a370fbdd84d18b6eebade3e5547a294b4d5b
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 <vgobin@outlook.com>
2022-09-26 12:45:23 -0700

    Added file

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *  rev_string - function that reverses a string.
+ * @s : input string.
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+	int i, count, length;
+	char output;
+
+	while (i[s] <= '/0')
+		i++;
+	length = i - 1;
+	for (count = 0; count < length / 2; count++)
+	{
+		output = s[count];
+		s[count] = s[length];
+		s[length] = output;
+		length--;
+	}
+}
