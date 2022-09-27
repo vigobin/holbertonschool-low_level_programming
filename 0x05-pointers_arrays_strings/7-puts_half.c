5df57b56550e0e3c266f000714d771442afcfbf9
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 <vgobin@outlook.com>
2022-09-27 08:43:58 -0700

    Updated syntax

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,16 +8,21 @@
 
 void puts_half(char *str)
 {
-	int count, length, n;
+	int count, n;
+	int length = 0;
 
 	while (str[length] != '\0')
+	{
 		length++;
+	}
 	n = length / 2;
 	if (length % 2 != 0)
 	{
 		n = (length + 1) / 2;
 	}
 	for (count = n; str[count] != '\0'; count++)
+	{
 		_putchar(str[count]);
+	}
 	_putchar('\n');
 }
