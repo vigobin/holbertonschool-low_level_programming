9630f17829d99871f50d6b336e555d783c447016
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 <vgobin@outlook.com>
2022-09-27 10:34:27 -0700

    Updated from if to while loop

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,9 +12,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int count;
 
-	for (count = 0; src[count] == '\0'; count++)
+	while (src[count])
 	{
 		dest[count] = src[count];
+		count++;
 	}
 	return (dest);
 }
