f45a8b93a66fe5b57c5916b8c66c87b5f5ad85f7
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 <vgobin@outlook.com>
2022-09-27 10:41:09 -0700

    Updated to for loop

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,12 +10,12 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int count = 0;
+	int count;
 
-	while (src[count])
+	for (count = 0; src[count] != '\0'; count++)
 	{
 		dest[count] = src[count];
-		count++;
 	}
+	dest[count] = '\0';
 	return (dest);
 }
