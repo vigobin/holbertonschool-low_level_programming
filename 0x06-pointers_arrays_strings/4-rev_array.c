0e47ca1257652f549b54c4d869c1f6d6e134e940
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 <vgobin@outlook.com>
2022-09-29 13:48:38 -0700

    Added file for task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *reverse_array - Function that reverses the content of an array of integers.
+ *@a: array of integers.
+ *@n: numner of elements to swap.
+ */
+
+void reverse_array(int *a, int n)
+{
+	int count, temp;
+
+	for (count = 0; a[n]; count++)
+	{
+		n--;
+		temp = a[count];
+		a[count] = a[n];
+		a[n] = temp;
+	}
+}
