a8ca60180d176b3106e4382773b2b36a40cbf31c
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 <vgobin@outlook.com>
2022-10-03 14:07:54 -0700

    Updated code with unsigned int

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *memset - Function that fills memory with a constant byte.
+ *_memset - Function that fills memory with a constant byte.
  *@s: memory variable.
  *@b: constant byte.
  *@n: first n bytes to be filled.
@@ -10,12 +10,12 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
+	unsigned int i = 0;
 
 	while (i < n)
 	{
 		s[i] = b;
-		count++;
+		i++;
 	}
 	return (s);
 }
