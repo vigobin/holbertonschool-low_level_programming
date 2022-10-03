670530fcdc649d0c9f90ff21d65519f653246396
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 <vgobin@outlook.com>
2022-10-03 08:53:05 -0700

    Added file

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *string_toupper - function that changes all lowercase letters of a string
+ *to uppercase.
+ *@str: input string.
+ *Return: str for uppercase.
+ */
+
+char *string_toupper(char *str)
+{
+	int count = 0;
+
+	while (str[count])
+	{
+		count++;
+		if (str[count] >= 'a' && str[count] >= 'z')
+			str[count] = str[count] - 32;
+	}
+	return (str);
+}
