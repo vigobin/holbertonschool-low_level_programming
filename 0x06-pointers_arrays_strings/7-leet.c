e48cc670af9fa3ee5512e2d958d5ad9843877e5d
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 <vgobin@outlook.com>
2022-10-03 10:59:30 -0700

    Added file

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *leet - function that encodes a string into 1337.
+ *@str: input str
+ *Return: updated string.
+ */
+
+char *leet(char *str);
+{
+	int count, count2;
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (count = 0; str[count]; count++)
+	{
+		for (count2 = 0, count2 < 10; count2++)
+		{
+			if (str[count] == a[count2])
+			{
+				str[count] = b[count2];
+			}
+		}
+	}
+	return (str);
+}
