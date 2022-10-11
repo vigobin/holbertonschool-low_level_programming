322b5573bab45622a6a96f9f33334d89d6567938
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 <vgobin@outlook.com>
2022-10-11 13:58:11 -0700

    Added file for task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ **str_concat - function that concatenates two strings.
+ *@s1: first string input.
+ *@s2: second string.
+ *Return: pointer to new allocated memory containing both strings
+ *NULL on failure.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *p;
+	int i, len, len1, len2;
+
+	if (s1 == NULL || s2 == NULL)
+		s1 = s2 = "";
+
+	len1 = 0;
+	while (s1[len1])
+		len1++;
+	len2 = 0;
+	while (s2[len2])
+		len2++;
+
+	len = len1 + len2;
+	p = malloc(len * sizeof(char));
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i]; i++)
+		p[i] = s1[i];
+	for (i = 0; s2[i]; i++)
+		p[i] = s2[i];
+
+	return (p);
+}
