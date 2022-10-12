875aaa2ecb25ce5bdcdf8d16d10d408161fc125d
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 <vgobin@outlook.com>
2022-10-11 18:59:52 -0700

    Added file for task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *free_grid - function that frees a 2 dimensional grid.
+ *@grid: array input to free.
+ *@height: height of array.
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+}
