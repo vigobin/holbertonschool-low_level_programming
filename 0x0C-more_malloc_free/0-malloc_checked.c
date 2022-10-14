9702821df26d1ed4d2039696cae5f34fdcd3f5ad
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 <vgobin@outlook.com>
2022-10-14 07:23:10 -0700

    Added file for task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *malloc_checked - function that allocates memory using malloc.
+ *@b: size input.
+ *Return: pointer to allocated memory or exit with 98 if NULL.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(sizeof((*ptr) * b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
