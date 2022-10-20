cdd6ba195b4eb7828fd385d1b043cb02080c2dba
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 <vgobin@outlook.com>
2022-10-20 11:34:08 -0700

    Added header file for task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - new struct.
+ *@name: First member.
+ *@age: Second member.
+ *@owner: Third member.
+ *
+ *Description: new struct named dog with char and float types.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
