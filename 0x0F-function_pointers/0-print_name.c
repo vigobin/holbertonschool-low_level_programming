5d94e9ca46919283d93d1e38fada215f8ac7ac40
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 <vgobin@outlook.com>
2022-10-24 07:59:51 -0700

    Updated to check for Null

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "function_pointers.h"
 
@@ -6,10 +5,12 @@
  *print_name - function that prints a name.
  *@name: name input.
  *@f: argument for pointer to function.
- *Return: 0
+ *Return: exit if NULL is passed.
  */
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (!name || !f)
+		return;
 	f(name);
 }
