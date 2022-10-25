63845351093f09faeabdd9552b1add4807574817
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 <vgobin@outlook.com>
2022-10-25 11:15:09 -0700

    Updated function

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -6,10 +6,22 @@
  *get_op_func - function that selects the correct function to perform
  *the operation asked by the user.
  *@s: operator passed as argument to the program.
- *Return: NULL for failure.
+ *Return: NULL for no match.
  */
 
 int (*get_op_func(char *s))(int, int)
 {
+	op_t opsel[];
+	int i;
 
+	opsel = {{"+", op_add}, {"-", op_sub},
+		{"*", op_mul}, {"/", op_div}, {"%", op_mod}};
+
+	while (opsel[i].op)
+	{
+		if (*(opsel[i].op) == *s)
+			return (op_s[i].f);
+		i++;
+	}
+	return (NULL);
 }
