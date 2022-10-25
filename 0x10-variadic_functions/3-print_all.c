43ac28f6686bc44a36de7b00b329b8ab2a3fa74d
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 <vgobin@outlook.com>
2022-10-25 13:07:21 -0700

    Added file for task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ *print_all - function that prints anything.
+ *@format: is a list of types of arguments passed to the function.
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	int i = 0;
+	char *str;
+
+	va_start(ap, format);
+
+	while (format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c, ", va_arg(ap, int));
+				break;
+			case 'i':
+				printf("%d, ", va_arg(ap, int));
+				break;
+			case 'f':
+				printf("%f, ", va_arg(ap, double));
+				break;
+			case 's':
+				str = va_arg(ap, char *);
+				if (!str)
+					str = "(nil)";
+				printf("%s", str);
+				break;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(ap);
+}
+
