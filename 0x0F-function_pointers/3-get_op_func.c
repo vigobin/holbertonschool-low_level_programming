2cecf05c00cf0a344fd187d8975feb67608ef889
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 <vgobin@outlook.com>
2022-10-25 10:48:02 -0700

    added files for task 3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ *get_op_func - function that selects the correct function to perform
+ *the operation asked by the user.
+ *@s: operator passed as argument to the program.
+ *Return: NULL for failure.
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+
+}
