b8e38e1a6e550c3ed819d13bae6ffcdd6cb8947c
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 <vgobin@outlook.com>
2022-10-26 12:48:26 -0700

    Updated due to output error of NULL

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,11 +13,9 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		printf("Name: (nil)\n");
-	if (d->age < 0)
-		printf("(nil)\n");
+		d->name = "(nil)";
 	if (d->owner == NULL)
-		printf("(nil)\n");
+		d->owner = "(nil)";
 
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
