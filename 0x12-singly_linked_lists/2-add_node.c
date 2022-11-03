bcf9e39e7abcaa55dc887aa95cee073e51acbc39
vigobin <vgobin@outlook.com>
2022-11-03 14:09:52 -0700

    Fixed betty style

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
-/*
- *add_node - function that adds a new node at the beginning of a list_t list.
- *@head: header node pointer.
- *@str: string to be duplicated.
- *Return: The address of the new element, or NULL if it failed.
+/**
+ * add_node - function that adds a new node at the beginning of a list_t list.
+ * @head: header node pointer.
+ * @str: string to be duplicated.
+ * Return: The address of the new element, or NULL if it failed.
  */
 
 list_t *add_node(list_t **head, const char *str)
@@ -12,13 +12,14 @@ list_t *add_node(list_t **head, const char *str)
 	char *copy;
 	int len = 0;
 
-	list_t* newnode = malloc(sizeof(list_t));
+	list_t *newnode = malloc(sizeof(list_t));
+
 	if (!newnode)
 		return (NULL);
 
 	copy = strdup(str);
 
-	while(str[len])
+	while (str[len])
 		len++;
 
 	newnode->str = copy;
