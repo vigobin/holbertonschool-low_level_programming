1a63813d9ed3fa5dcfa38c23a608dce81134dd69
vigobin <vgobin@outlook.com>
2022-11-04 14:10:38 -0700

    Added file for task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a listint_t list.
+ *  function sets the head to NULL.
+ * @head: start of node.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *node;
+
+	while (*head)
+	{
+		node = *head;
+		*head = (*head)->next;
+		free(node);
+	}
+	head = NULL;
+}
