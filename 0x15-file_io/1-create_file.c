96182aef3c22fd036570ce863fea8867e2e69631
vigobin <vgobin@outlook.com>
2022-11-09 14:01:48 -0800

    Updated Null check

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	len = strlen(text_content);
+	if (text_content)
+		len = strlen(text_content);
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, len);
