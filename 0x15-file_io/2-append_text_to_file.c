abdefabbb1f650f56df42b1828a175cdd5bfb29f
vigobin <vgobin@outlook.com>
2022-11-09 14:14:06 -0800

    Updated if statement to check length

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,7 +13,10 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	len = strlen(text_content);
+	if (text_content)
+	{
+		len = strlen(text_content);
+	}
 
 	o = open(filename, O_WRONLY | O_APPEND);
 	w = write(o, text_content, len);
