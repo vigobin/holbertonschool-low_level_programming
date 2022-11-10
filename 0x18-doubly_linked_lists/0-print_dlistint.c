f436aabcc822e960b7e2edcf51a4515ed047bc8a
vigobin <vgobin@outlook.com>
2022-11-10 07:56:33 -0800

    Added file for task 0

---
+++ 0x18-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list.
+ * @h: head node pointer.
+ * Return: the number of nodes.
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t node_count = 0;
+
+	while (h)
+	{
+		node_count++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (node_count);
+}
