3a8ee00bc58573044847116fdf26599d048f1c96
vigobin <vgobin@outlook.com>
2022-11-14 13:04:18 -0800

    Added file for task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,40 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - function that adds an element to the hash table.
+ * @ht: the hash table you want to add or update the key/value to.
+ * @key: the key of the hash table, it cannot be an empty string.
+ * @value: value associated with the key. value must be duplicated.
+ *  value can be an empty string.
+ *  Return: 1 if it succeeded, 0 otherwise.
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index = 0;
+	hash_node_t *new_node, *element;
+
+	if (!ht || !key || !value)
+		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+	element = ht->array[index];
+
+	if (element && strcmp(key, element->key) == 0)
+	{
+		free(element->value);
+		element->value = strdup(value);
+		return (1);
+	}
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
+}
